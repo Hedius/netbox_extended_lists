53ed6e1353fa6d90f2a13f5c216d816a936a8984
Florian Derler <f.derler@conova.com>
2024-04-04 15:21:05 +0200

    fix plugin name

--- netbox_extended_lists/__init__.py
+++ netbox_extended_lists/__init__.py
@@ -1,12 +1,12 @@
 from extras.plugins import PluginConfig
 
 
-class NetBoxExtendedPrefixList(PluginConfig):
+class NetBoxExtendedLists(PluginConfig):
     name = 'netbox_extended_lists'
-    verbose_name = ' NetBox Extended Prefix List'
+    verbose_name = ' NetBox Extended Lists'
     description = 'Shows all prefixes and their IP addresses for a filtered view.'
     version = '0.1'
-    base_url = 'prefix-list'
+    base_url = 'extended-lists'
 
 
-config = NetBoxExtendedPrefixList
+config = NetBoxExtendedLists
